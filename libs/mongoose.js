dca189b35c20b615c0c796209eb2f7b9459eb8e6
Patrickodey360coder <patrickodey419@gmail.com>
2024-06-24 23:48:59 +0100

    fixed connection issues

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,26 +1,11 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mrrobot:mrrobot@cluster0.7zm6chn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+import mongoose from "mongoose";
+const uri = process.env.MONGODB_URI
 
-//Create a MongoClient with a MongoCLientOptions object to set the Stable API Version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true
-  }
-});
-
-export default async function run() {
+export default async function dbConnect() {
   try {
-    //Connect the client to the server
-    await client.connect();
-    //send a ping to confirm a successful connection
-    await client.db("test").command({ ping: 1 });
-    console.log("Pingged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close()
+    await mongoose.connect(uri)
+    console.log("database has been connected")
+  } catch(error) {
+    handleError(error)
   }
 }
-
-run().catch(console.dir);
--- models/Contacts.js
+++ models/Contacts.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const {model, Schema} = mongoose
 
-const ContactSchema = new Schema({
+const ContactSchema = new mongoose.Schema({
   firstname: {type: String, required: true},
-  secondname: {type: String, required: true},
+  lastname: {type: String, required: true},
   email: {type: String, required: true},
-  message: {type: String, required: true}
-});
+  message: {type: String, required: true},
+  
+}, { timestamps: true });
 
-const Contact = model("Contact", ContactSchema  )
-export default Contact
+
+export default mongoose.models.Contact || mongoose.model('Contact', ContactSchema)
