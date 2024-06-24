f3db2d0ad75507bc5dae34729e077d88d1ac1825
Patrickodey360coder <patrickodey419@gmail.com>
2024-06-24 11:51:32 +0100

    api routes

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -6,10 +6,10 @@ function Contact() {
       <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8"> 
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-[#42446E] sm:text-4xl">
-            Contact sales
+            Contact Me
           </h2>
           <p className="mt-2 text-lg leading-8 text-[#666666]">
-            Aute magna irure deserunt veniam aliqua magna enim voluptate.
+            Send me a message here.
           </p>
         </div>
         <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
@@ -24,7 +24,7 @@ function Contact() {
                   name="first-name"
                   id="first-name"
                   autoComplete="given-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
