c252493ef340d9a9a2c1d44b4ffd497670675c52
Patrickodey360coder <patrickodey419@gmail.com>
2024-06-28 14:25:35 +0100

    Zod validation

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,24 +1,60 @@
 "use server"
 import dbConnect from "../../../libs/mongoose";
 import Contact from "../../../models/Contact";
+import { z } from 'zod'
 
 dbConnect();
 
-const contactForm = async ( formData: FormData ) => {
+const schema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  firstname: z.string({
+    message: "firstname is required",
+    required_error: "firstname is required",
+    invalid_type_error: "firstname must be a string"
+  }),
+  lastname: z.string({
+    message: "lastname is required",
+    required_error: "lastname is required",
+    invalid_type_error: "lastname must be a string"
+  }),
+  message: z.string({
+    message: "Message is required",
+    required_error: "Message is required",
+    invalid_type_error: "message must be a string"
+  }),
+});
+
+const contactForm = async (prevState: any, formData: FormData ) => {
   try {
-  
       const rawFormData = {
         firstname: formData.get('firstname'),
         lastname: formData.get('lastname'),
         email: formData.get('email'),
         message: formData.get('message')
       }
-  
-      const contact = new Contact(rawFormData)
+
+      const validationResult = schema.safeParse(rawFormData); 
+      
+      if (!validationResult.success) {
+        return {
+          errors: validationResult.error.flatten().fieldErrors
+        };
+      }
+      const validatedData = validationResult.data
+
+      const contact = new Contact(validatedData)
       await contact.save()
 
+      return {
+        message: "Form Submitted Successfully"
+      }
+
   } catch (error) {
-    console.log(error)
+      return {
+        errors: { server: "An error occurred while submitting the form" }
+      };
   }
 }
 
--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,7 +1,16 @@
+"use client"
 import React from 'react';
 import Contact from '../actions/actions'
+import SubmitButton from './button';
+import { useFormState } from 'react-dom'; 
+
+const initialState = {
+  message: '',
+}
 
 async function contactForm() {
+
+  const [ state, formAction ] = useFormState(Contact, initialState)
 
   return (
     <section>
@@ -14,7 +23,8 @@ async function contactForm() {
             Send me a message here.
           </p>
         </div>
-        <form action={Contact} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+
+        <form action={formAction} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div>
               <label htmlFor="firstname" className="block text-sm font-semibold leading-6 text-[#42446E]">
@@ -50,12 +60,13 @@ async function contactForm() {
               </label>
               <div className="mt-2.5">
                 <input
-                  type="email"
+                  // type="email"
                   name="email"
                   id="email"
                   autoComplete="email"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                <small>{state?.message}</small>
               </div>
             </div>
 
@@ -75,12 +86,7 @@ async function contactForm() {
 
           </div>
           <div className="mt-10">
-            <button
-              type="submit"
-              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Let's talk
-            </button>
+            <SubmitButton />
           </div>
         </form>
       </div>
