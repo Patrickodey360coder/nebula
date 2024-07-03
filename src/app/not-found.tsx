f7ddfacd7685fd2ed93edcc6112a3d040fbae55b
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-03 09:07:44 -0500

    404.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,10 +9,10 @@ const NotFound = () => {
   return (
     <div className='h-screen'>
       <Nav/>
-      <section className='p-4 max-w-[1640px] mx-auto'>
-        <div className='grid border-4 border-black place-items-center'>
-          <h2>Not Found</h2>
-          <p>Could not find requested resource</p>
+      <section className='p-4 max-w-[1640px] mx-auto h-[84%] grid place-items-center'>
+        <div className='border-4 border-black text-center p-6 rounded-lg'>
+          <h2 className='text-2xl font-bold text-[#42446E]'>Page Not Found!!!</h2>
+          <p className='my-4'>Could not find requested resource</p>
           <Link href="/" className={ buttonVariants({ variant: "outline" }) }>Go back to Homepage</Link>
         </div>
       </section>
