ae33f4908a659c9fac74f8129491a7c812ea9460
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-03 08:41:27 -0500

    added button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,19 +2,22 @@ import React from 'react'
 import Link from 'next/link'
 import Nav from './components/nav'
 import Footer from './components/footer'
+import { buttonVariants } from '@/components/ui/button'
 
 
 const NotFound = () => {
   return (
-    <>
+    <div className='h-screen'>
       <Nav/>
-      <section>
-        <h2>Not Found</h2>
-        <p>Could not find requested resource</p>
-        <Link href="/">Return Home</Link>
+      <section className='p-4 max-w-[1640px] mx-auto'>
+        <div className='grid border-4 border-black place-items-center'>
+          <h2>Not Found</h2>
+          <p>Could not find requested resource</p>
+          <Link href="/" className={ buttonVariants({ variant: "outline" }) }>Go back to Homepage</Link>
+        </div>
       </section>
       <Footer/>
-    </>
+    </div>
   )
 }
 
