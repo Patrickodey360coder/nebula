fdf04c998e8ba29c829cc261ba98c45b7de9a6d0
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-05 14:40:28 -0500

    validated actions

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -7,9 +7,13 @@ import { useFormState } from 'react-dom';
 const initialState = {
   message: '',
   errors: {
-    server: ''
+    server: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    message: '',
   }
-}
+};
 
 const ContactForm = () => {
 
@@ -41,6 +45,7 @@ const ContactForm = () => {
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
                 />
+                { state?.errors?.firstname && <small className='text-red-600'>{ state?.errors?.firstname }</small> }
               </div>
             </div>
             <div>
