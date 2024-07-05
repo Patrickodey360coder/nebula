ff9c209cbbf72a1ae67ee44b5041282ca1ac3d6e
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-04 21:54:08 -0500

    zod validation

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -3,30 +3,23 @@ import dbConnect from "../../../libs/mongoose";
 import Contact from "../../../models/Contact";
 import { z } from 'zod'
 
-dbConnect();
-
 const schema = z.object({
   email: z.string().email({
     message: "Email is required",
   }),
-  firstname: z.string({
-    message: "firstname is required",
-    required_error: "firstname is required",
-    invalid_type_error: "firstname must be a string"
+  firstname: z.string().min(2, {
+    message: "firstname is required"
   }),
-  lastname: z.string({
-    message: "lastname is required",
-    required_error: "lastname is required",
-    invalid_type_error: "lastname must be a string"
+  lastname: z.string().min(2, {
+    message: "Last name is required"
   }),
-  message: z.string({
-    message: "Message is required",
-    required_error: "Message is required",
-    invalid_type_error: "message must be a string"
+  message: z.string().min(2, {
+    message: "Message field is required"
   }),
 });
 
 const contactForm = async (prevState: any, formData: FormData ) => {
+  await dbConnect();
   try {
       const rawFormData = {
         firstname: formData.get('firstname'),
