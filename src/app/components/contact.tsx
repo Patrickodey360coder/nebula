c994909e01f7af429ad89e2f84460be7bea9fc54
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-05 15:08:37 -0500

    added success alert

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -36,6 +36,16 @@ const ContactForm = () => {
           </p>
         </div>
 
+        {state.message && (
+          <div className="mx-auto mt-4 max-w-xl sm:mt-6">
+            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+              <span className="block sm:inline">{state.message}</span>
+            </div>
+          </div>
+        )}
+
+
+
         <form action={formAction} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div>
@@ -92,9 +102,10 @@ const ContactForm = () => {
                 <textarea
                   name="message"
                   id="message"
-                  rows="4"
+                  rows={4}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                { state?.errors?.message && <small className='text-red-600'>{ state.errors.message }</small> }
               </div>
             </div>
 
