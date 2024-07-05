7268fc365157077cbc3fe41caec1c8edc000ed55
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-04 22:26:08 -0500

    fixed zod resolver

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -6,6 +6,7 @@ import { useFormState } from 'react-dom';
 
 const initialState = {
   message: '',
+  errors: []
 }
 
 const ContactForm = () => {
@@ -67,6 +68,7 @@ const ContactForm = () => {
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <small>{state?.message}</small>
+
               </div>
             </div>
 
