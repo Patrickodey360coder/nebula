37663b14fa27852ba65c0c7e938ec0b57efea242
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-07 13:38:59 -0500

    form test

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,25 @@
+"use client"
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
-const page = () => {
+const Page = () => {
+  const { 
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues
+   } = useForm()
   return (
-    <form className='flex flex-col gap-y-2'>
+    <form className='flex flex-col gap-y-2 py-4'>
         <input type="email" placeholder='email' className='px-4 py-2 rounded' />
-        <input type="email" placeholder='email' className='px-4 py-2 rounded' />
-        <input type="email" placeholder='email' className='px-4 py-2 rounded' />
-        input
+        <input type="password" placeholder='password' className='px-4 py-2 rounded' />
+        <input type="password" placeholder='email' className='px-4 py-2 rounded' />
+        
+        <Button variant={'outline'}>Submit</Button>
     </form>
   )
 }
 
-export default page
+export default Page
