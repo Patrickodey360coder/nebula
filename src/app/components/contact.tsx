c6b421a4adca82fd6b0f6006cbab82659941f5b6
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-08 18:58:09 -0500

    fixed validation

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState, useRef } from 'react';
 import Contact from '../actions/actions'
 import SubmitButton from './button';
 import { useFormState } from 'react-dom'; 
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
+
 
 type FormState = {
   message: string;
@@ -16,6 +17,13 @@ type FormState = {
   }
 }
 
+type FormTypes = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  message: string
+}
+
 const initialState: FormState = {
   message: '',
   errors: {}
@@ -25,33 +33,34 @@ const ContactForm = () => {
 
   const { 
     register, 
-    handleSubmit, 
-    reset,
-    formState: { errors } 
-  } = useForm();
+    handleSubmit,
+    formState: { errors }, 
+    reset } = useForm();
   const [ state, formAction ] = useFormState<typeof initialState>(Contact, initialState);
-  // const [ showAlert, setShowAlert ] = useState(false);
+  const [ showAlert, setShowAlert ] = useState(false);
   const formRef = useRef(null)
 
-  // useEffect(() => {
-  //   if (state.message) {
-  //     setShowAlert(true);
-  //   }
-  // },[state.message]);
+  useEffect(() => {
+    if (state.message) {
+      setShowAlert(true);
+    }
+  },[state.message]);
 
-  // const resetForm = () => {
-  //   if(formRef.current) {
-  //     formRef.current.reset();
-  //   }
-  //   formAction()
-  // }
+  const resetForm = () => {
+    if(formRef.current) {
+      formRef.current.reset();
+    }
+    formAction()
+  }
 
-  // const handleCloseAlert = () => {
-  //   setShowAlert(false);
-  //   resetForm()
-  // };
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+    resetForm()
+  };
 
-  const onSubmit = (data: unknown) => console.log(data)
+  const onSubmit = async (data: FieldValues) => { 
+    console.log(data)
+  }
 
   return (
     <section className='max-w-[1640px] mx-auto'>
@@ -65,45 +74,7 @@ const ContactForm = () => {
           </p>
         </div>
 
-        {/* {showAlert && (
-          <div className="mx-auto mt-4 max-w-xl sm:mt-6">
-            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-              <span className="block sm:inline">{state.message}</span>
-              <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg
-                  className="fill-current h-6 w-6 text-green-500"
-                  role="button"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  onClick={handleCloseAlert}
-                >
-                  <title>Close</title>
-                  <path d="M14.348 5.652a1 1 0 010 1.414L11.414 10l2.934 2.934a1 1 0 01-1.414 1.414L10 11.414l-2.934 2.934a1 1 0 01-1.414-1.414L8.586 10 5.652 7.066a1 1 0 011.414-1.414L10 8.586l2.934-2.934a1 1 0 011.414 0z" />
-                </svg>
-              </span>
-            </div>
-          </div>
-        )} */}
-
-        {/* {showAlert && (
-          <div className="mx-auto mt-4 max-w-xl sm:mt-6">
-            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-              <span className="block sm:inline">{state.message}</span>
-              <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg
-                  className="fill-current h-6 w-6 text-green-500"
-                  role="button"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  onClick={handleCloseAlert}
-                >
-                  <title>Close</title>
-                  <path d="M14.348 5.652a1 1 0 010 1.414L11.414 10l2.934 2.934a1 1 0 01-1.414 1.414L10 11.414l-2.934 2.934a1 1 0 01-1.414-1.414L8.586 10 5.652 7.066a1 1 0 011.414-1.414L10 8.586l2.934-2.934a1 1 0 011.414 0z" />
-                </svg>
-              </span>
-            </div>
-          </div>
-        )} */}
+ 
 
         <form onSubmit={handleSubmit(onSubmit)} action={formAction} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
@@ -112,17 +83,20 @@ const ContactForm = () => {
                 First name
               </label>
               <div className="mt-2.5">
-                <input
-                  {...register("firstname"), {
-                    required: true,
-                    minLength: 2
-                  }}
+                <input 
+                  {...register("firstname", {
+                    required: "firstname is required",
+                    minLength: {
+                      value: 2,
+                      message: "Cannot use less than 2 characters"
+                    }
+                  })}
                   type="text"
-                  name="firstname"
                   id="firstname"
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
                 />
+                {errors.firstname && <small className='text-read-500'>{errors.firstname.message}</small> }
               </div>
             </div>
             <div>
@@ -131,10 +105,13 @@ const ContactForm = () => {
               </label>
               <div className="mt-2.5">
                 <input
-                  {...register("lastname"), {
-                    required: true,
-                    minLength: 2
-                  }}
+                  {...register("lastname", {
+                    required: "lastname is required",
+                    minLength: {
+                      value: 2,
+                      message: "Cannot use less than 2 characters"
+                    }
+                  })}
                   type="text"
                   name="lastname"
                   id="lastname"
@@ -149,7 +126,13 @@ const ContactForm = () => {
               </label>
               <div className="mt-2.5">
                 <input
-                  {...register("email")}
+                  {...register("email", {
+                    required: "email is required",
+                    minLength: {
+                      value: 2,
+                      message: "Cannot use less than 2 characters"
+                    }
+                  })}
                   type="email"
                   name="email"
                   id="email"
@@ -165,7 +148,13 @@ const ContactForm = () => {
               </label>
               <div className="mt-2.5">
                 <textarea
-                  {...register("message")}
+                  {...register("message", {
+                    required: "message is required",
+                    minLength: {
+                      value: 2,
+                      message: "Cannot use less than 2 characters"
+                    }
+                  })}
                   name="message"
                   id="message"
                   rows={4}
@@ -180,76 +169,6 @@ const ContactForm = () => {
           </div>
         </form>
 
-
-
-        {/* <form ref={formRef} action={formAction} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div>
-              <label htmlFor="firstname" className="block text-sm font-semibold leading-6 text-[#42446E]">
-                First name
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="firstname"
-                  id="firstname"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
-                />
-                { state?.errors?.firstname && <small className='text-red-600'>{ state.errors.firstname }</small> }
-              </div>
-            </div>
-            <div>
-              <label htmlFor="lastname" className="block text-sm font-semibold leading-6 text-[#42446E]">
-                Last name
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="lastname"
-                  id="lastname"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                { state?.errors?.lastname && <small className='text-red-600'>{ state.errors.lastname }</small> }
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-[#42446E]">
-                Email
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                { state?.errors?.email && <small className='text-red-600'>{ state.errors.email }</small> }
-              </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label htmlFor="message" className="block text-sm font-semibold leading-6 text-[#42446E]">
-                Message
-              </label>
-              <div className="mt-2.5">
-                <textarea
-                  name="message"
-                  id="message"
-                  rows={4}
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                { state?.errors?.message && <small className='text-red-600'>{ state.errors.message }</small> }
-              </div>
-            </div>
-
-          </div>
-          <div className="mt-10">
-            <SubmitButton />
-          </div>
-        </form> */}
       </div>
     </section>
   );
