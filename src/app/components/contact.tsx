30f7d213f041631368aed717e831d7b142ab1633
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-11 20:53:55 +0100

    mm

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -149,7 +149,7 @@ const ContactForm = () => {
                   })}
                   name="message"
                   id="message"
-                  rows={4}
+                  rows={5}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-[#42446E] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 {errors.message && <small className='text-red-500'>{errors.message.message as string}</small> }
