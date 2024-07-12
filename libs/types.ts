3115d5b30358f756266d08fab0a5fc2618882273
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-12 18:18:23 -0500

    add new blog

--- libs/types.ts
+++ libs/types.ts
@@ -15,4 +15,19 @@ export const signUpSchema = z.object({
   })
 }); 
 
+
 export type TSignUpSchema = z.infer<typeof signUpSchema>
+
+export const BlogSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title is required..."
+  }),
+  author: z.string().min(2, {
+    message: "Author is required..."
+  }),
+  content: z.string().min(10, {
+    message: "Content is required..."
+  })
+});
+
+export type TBlogSchema = z.infer<typeof BlogSchema>
