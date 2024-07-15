14aaaf32076c1db2173a19f6d3edc86db26f62a0
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-15 14:36:24 -0500

    changed navbar colors

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -15,13 +15,13 @@ function About() {
 
   return (
     <section className="max-w-[1640px] p-4 mx-auto" id="about">
-      <h3 className="text-2xl text-[#42446E] font-extrabold my-10 text-center">About</h3>
+      <h3 className="text-2xl font-extrabold my-10 text-center">About</h3>
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="image mx-auto">
           <Lottie options={defaultOptions} height={400} width={300} />
         </div>
 
-        <div className="description text-[#666666] md:p-5">
+        <div className="description md:p-5">
           <h3 className='my-3 font-bold text-center sm:text-center mx-4 md:text-center'> Who is this guy?</h3>
           <p className='my-3'>I am a dedicated and accomplished software engineer with over 4 years of experience in the industry. Throughout my career, I have consistently demonstrated my expertise in various frontend and backend technologies, contributing to numerous successful projects. My journey has been marked by a continuous drive for learning and improvement, enabling me to become proficient in modern frameworks and tools.</p>
 
--- src/app/components/tech-stack.tsx
+++ src/app/components/tech-stack.tsx
@@ -14,7 +14,7 @@ import Image from "next/image";
 function TechStack() {
   return (
     <section className='max-w-[1640px] p-4 mx-auto' id="#techstack">
-      <h3 className='text-center text-2xl text-[#42446E] font-extrabold my-10'>My Tech Stack</h3>
+      <h3 className='text-center text-2xl font-extrabold my-10'>My Tech Stack</h3>
 
       <div className='w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-y-6 mt-5 '>
         <div className="text-5xl text-[#00D8FF]"><FaReact /></div>
@@ -22,8 +22,8 @@ function TechStack() {
         <div className="text-5xl text-[#3078C6]"><SiTypescript /></div>
         <div className="text-5xl text-[#663399]"><GrGatsbyjs /></div>
         <div className="text-5xl text-[#5FA04F]"><FaNode /></div>
-        <div className="text-5xl text-[#000000]"><SiFastify /></div>
-        <div className="text-5xl text-[#0D1116]"><SiExpress /></div>
+        <div className="text-5xl"><SiFastify /></div>
+        <div className="text-5xl"><SiExpress /></div>
         <div className="text-5xl text-[#5967D8]"><SiPrisma /></div>
         <div className="text-5xl text-[#01ED64]"><SiMongodb /></div>
         <div className=""><Image src='/images/harperdb.png' height={80} width={80} alt="HaperDb"/></div>
