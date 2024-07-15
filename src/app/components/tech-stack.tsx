21e8628a68c4a49bf9deb2224bc99b68e3342e1a
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-15 14:37:13 -0500

    changed tech stack

--- src/app/components/tech-stack.tsx
+++ src/app/components/tech-stack.tsx
@@ -18,7 +18,7 @@ function TechStack() {
 
       <div className='w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-y-6 mt-5 '>
         <div className="text-5xl text-[#00D8FF]"><FaReact /></div>
-        <div className="text-5xl text-[#000000]"><SiNextdotjs /></div>
+        <div className="text-5xl"><SiNextdotjs /></div>
         <div className="text-5xl text-[#3078C6]"><SiTypescript /></div>
         <div className="text-5xl text-[#663399]"><GrGatsbyjs /></div>
         <div className="text-5xl text-[#5FA04F]"><FaNode /></div>
