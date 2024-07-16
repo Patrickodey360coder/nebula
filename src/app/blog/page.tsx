48faf97cc0b8ca2b831dfaef71bb0b58dedc278f
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-16 21:58:12 +0100

    article

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,8 +5,12 @@ import Article from '../components/article'
 const Page = () => {
   return (
     <section className='max-w-[1640px] mx-auto'>
-      <h1 className='text-2xl text-center font-bold'>Articles</h1>
-      <div>
+      <h1 className='text-2xl text-center font-bold my-5'>Articles</h1>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        <Article/>
+        <Article/>
+        <Article/>
+        <Article/>
         <Article/>
       </div>
     </section>
