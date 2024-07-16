9f5815cdbb1519be6716e05829fe8bf4c85b8815
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-16 21:33:09 +0100

    articles page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,6 @@ const Page = () => {
   return (
     <section className='max-w-[1640px] mx-auto'>
       <h1 className='text-2xl text-center font-bold'>Articles</h1>
-
       <div>
         <Article/>
       </div>
--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 function Article() {
+  const img = "https://media.istockphoto.com/id/77931645/photo/woman-and-young-girl-outdoors-with-people-in-background.webp?b=1&s=170667a&w=0&k=20&c=Pu8YKWG63QkM57TDJlOj1STNtHEGlc9LA-yocJM986A=";
   return (
-    <div className='p-2 border'>
-      <img src="" alt="" />
-      <h1>Technical Writing Template</h1>
-      <p>One of the things new and emerging writers struggle with is how to structure their content, images, or even code formatting.</p>
-      <small>Feb 2nd, 9:39 pm</small>
-      <small>Read more...</small>
+    <div className="max-w-sm rounded overflow-hidden shadow-md">
+      <Image src={img} width={200} height={200} alt="" />
+
+      <div className="px-4 md:px-4 py-2 md:py-4">
+        <h3 className="font-bold text-xl my-2">Technical Writing Template</h3>
+        <p className="text-sm my-2">
+        One of the things new and emerging writers struggle with is how to structure their content, images, or even code formatting.
+        </p>
+
+        <div className='flex justify-between'>
+          <Link href="" target="_blank">
+            <Button variant="outline">Preview</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
