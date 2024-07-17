853d56cf25080a5510969452884ebd61a079921e
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-18 00:25:52 +0100

    projects fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
       <Nav/>
       <section className='p-4 max-w-[1640px] mx-auto h-[84%] grid place-items-center'>
         <Card className='text-center p-6 rounded-lg'>
-          <h2 className='text-2xl font-bold text-[#42446E]'>Page Not Found!!!</h2>
+          <h2 className='text-2xl font-bold'>Page Not Found!!!</h2>
           <p className='my-4'>Could not find requested resource</p>
           <Link href="/" className={ buttonVariants({ variant: "outline" }) }>Go back to Homepage</Link>
         </Card>
