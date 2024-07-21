b38e8ab9b1847f17dee5488bdd2a0d34d694e2fa
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-21 02:55:58 +0100

    framer motion

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,6 +5,7 @@ import Typed from 'typed.js';
 import Lottie from 'react-lottie';
 import animationData from '../../lottie/hero.json';
 import Link from 'next/link';
+import { color, motion } from 'framer-motion'
 
 function Hero() {
   const defaultOptions = {
@@ -32,7 +33,15 @@ function Hero() {
   return (
     <section className="min-h-[600px] max-w-[1280px] sm:min-h-[500px] md:min-h-[700px] w-full p-4 flex flex-cols-2 mx-auto">
       <div className='w-full flex items-center'>
-        <div className="w-full">
+        <motion.div 
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.9,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
+          className="w-full">
           <h3 className='text-2xl md:text-3xl lg:text-4xl font-extrabold'>Hi 👋,</h3>
           <h1 className='text-4xl md:text-4xl lg:text-7xl my-2 font-extrabold'>I am Patrick Odey</h1>
           <h3 className='text-2xl md:text-2xl lg:text-3xl mb-1 font-extrabold'>Software & Developer Relations Engineer</h3>
@@ -47,15 +56,24 @@ function Hero() {
               <Button variant="outline">GitHub</Button>
             </Link>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       <div className='hidden w-full sm:hidden md:grid place-items-center'>
-        <div className='w-full object-cover'>
+        <motion.div   
+          initial={{ scale: 0 }}
+          animate={{ rotate: 360, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.1,
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+          }} className='w-full object-cover'>
           <Lottie
             options={defaultOptions} 
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   )
