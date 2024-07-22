006820ec6608764983aee8dabca5a3d1e2a73b92
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-22 10:34:40 +0100

    framer motion project card

--- src/app/components/project-card.tsx
+++ src/app/components/project-card.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -9,33 +10,44 @@ type ProjectCardProps = {
   description: string;
 }
 import { Card } from "@/components/ui/card"
+import { motion } from "framer-motion";
 
 function ProjectCard({title, picture, preview, github, description}: ProjectCardProps) {
   return (
-    <Card className="max-w-sm rounded overflow-hidden shadow-md">
-      <Image src={picture} alt={title} width={500} height={500} />
+    <motion.div         
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity:1 }} 
+      transition={{
+        duration: 0.7,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+      viewport={{ once: true }}>
+        <Card className="max-w-sm rounded overflow-hidden shadow-md">
+          <Image src={picture} alt={title} width={500} height={500} />
 
-      <div className="px-4 md:px-4 py-2 md:py-4">
-        <div className="font-bold text-xl my-2">{title}</div>
-        <p className="text-sm my-2">
-          {description}
-        </p>
+          <div className="px-4 md:px-4 py-2 md:py-4">
+            <div className="font-bold text-xl my-2">{title}</div>
+            <p className="text-sm my-2">
+              {description}
+            </p>
 
-        <div className='flex justify-between'>
-          <Link href={preview} target="_blank">
-            <Button variant="outline">Preview</Button>
-          </Link>
-
-          {
-            github && (
-              <Link href={github} target="_blank">
-                <Button variant="outline">Github</Button>
+            <div className='flex justify-between'>
+              <Link href={preview} target="_blank">
+                <Button variant="outline">Preview</Button>
               </Link>
-            )
-          }
-        </div>
-      </div>
-    </Card>
+
+              {
+                github && (
+                  <Link href={github} target="_blank">
+                    <Button variant="outline">Github</Button>
+                  </Link>
+                )
+              }
+            </div>
+          </div>
+        </Card>
+    </motion.div>
   )
 }
 
