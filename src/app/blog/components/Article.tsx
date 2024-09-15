62eff1591598a02fe2c229445cc2e0dfd8e6e5e5
Patrickodey360coder <patrickodey419@gmail.com>
2024-09-15 16:07:54 +0100

    blog

--- src/app/blog/components/Article.tsx
+++ src/app/blog/components/Article.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+
+type Props = {}
+
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
-function Article() {
+function Article({}: Props) {
   const img = "https://media.istockphoto.com/id/77931645/photo/woman-and-young-girl-outdoors-with-people-in-background.webp?b=1&s=170667a&w=0&k=20&c=Pu8YKWG63QkM57TDJlOj1STNtHEGlc9LA-yocJM986A=";
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-md">
       <Image src={img} style={{ width: '100%' }} width={100} height={150} alt="" />
-
       <div className="px-4 md:px-4 py-2 md:py-4">
         <h3 className="font-bold text-xl my-2">Technical Writing Template</h3>
         <p className="text-sm my-2">
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,12 @@
 "use client"
 import React from 'react'
-import Article from '../components/article'
+import Article from './components/Article'
 
 const Page = () => {
   return (
     <section className='max-w-[1640px] mx-auto'>
       <h1 className='text-2xl text-center font-bold my-5'>Articles</h1>
       <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        <Article/>
-        <Article/>
-        <Article/>
-        <Article/>
         <Article/>
       </div>
     </section>
